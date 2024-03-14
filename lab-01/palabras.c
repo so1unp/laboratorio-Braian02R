f7bb82e52210c58977807b2a46eab08cf2c3d8cb
Braian <bf02rojas.edu@gmail.com>
2024-03-14 18:58:53 -0300

    lab01-ejercicio3

--- lab-01/palabras.c
+++ lab-01/palabras.c
@@ -4,7 +4,16 @@
 int main(int argc, char *argv[])
 {
     // Agregar código aquí.
-
+	char  c;
+	//c = getchar();
+	while((c = getchar()) != EOF){
+		if(c == '\t' || c == ' ' || c =='\n'){
+                	printf("\n");
+      		}else{
+			putchar(c);
+		}	
+	
+	}
     // Termina la ejecución del programa.
     exit(EXIT_SUCCESS);
 }
