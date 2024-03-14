5bd78d0650bdf4db2a77ba452c76ca69abb53765
Braian <bf02rojas.edu@gmail.com>
2024-03-14 18:00:50 -0300

    lab01-ejercicio2

--- lab-01/sum.c
+++ lab-01/sum.c
@@ -4,7 +4,17 @@
 int main(int argc, char *argv[])
 {
     // Agregar código aquí.
+	int suma = 0;
+	int i = 1;
+    // Comenzamos desde el segundo argumento (índice 1) porque el primer argumento (índice 0) es el nombre del programa
+        for ( i; i < argc; i++) {
+                suma += atoi(argv[i]); // Convertimos la cadena a entero y sumamos
+              }
+
+    
+        printf("%i\n", suma);
 
     // Termina la ejecución del programa.
-    exit(EXIT_SUCCESS);
+        return(0);
+    // exit(EXIT_SUCCESS);
 }
