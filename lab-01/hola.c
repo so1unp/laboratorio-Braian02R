72e920d7f4a15f556d33b20439ccc5790a0350fe
Braian <bf02rojas.edu@gmail.com>
2024-03-14 18:59:38 -0300

    lab01-ejercicio01

--- lab-01/hola.c
+++ lab-01/hola.c
@@ -4,7 +4,13 @@
 int main(int argc, char *argv[])
 {
     // Agregar código aquí.
-	printf("hola mundo");
+    //	printf("hola mundo\n");
+
+	int i=1;
+	for(i; i<argc ; i++){
+		printf("%s ",argv[i]);
+	}
+	printf("\n");
     // Termina la ejecución del programa.
     exit(EXIT_SUCCESS);
 }
